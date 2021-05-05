a3348c31d774521e94f020f30447b1865e2fa843
bpmorgan-dev <69488134+bpmorgan-dev@users.noreply.github.com>
2021-05-05 16:55:49 -0500

    Added starting code

---
+++ equivalentresistancecircuitbuilding.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int N;
+    cin >> N; cin.ignore();
+    for (int i = 0; i < N; i++) {
+        string name;
+        int R;
+        cin >> name >> R; cin.ignore();
+    }
+    string circuit;
+    getline(cin, circuit);
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "Equivalent Resistance" << endl;
+}
